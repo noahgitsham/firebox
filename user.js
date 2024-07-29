880b38f431d5a52596de5d006c18f66b5ac84fd9
Noah Gitsham <nhgitsham@gmail.com>
2024-07-29 21:56:48 +0100

    Initial user.js

---
+++ user.js
@@ -0,0 +1,167 @@
+///////////////
+// BEHAVIOUR //
+///////////////
+user_pref("media.hardwaremediakeys.enabled", false);
+
+user_pref("browser.tabs.warnOnClose", true);
+
+user_pref("browser.aboutConfig.showWarning", false);
+
+// Fullscreen
+user_pref("browser.fullscreen.autohide", false);
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+
+// Autofill
+user_pref("browser.urlbar.unitConversion.enabled", true);
+user_pref("browser.urlbar.suggest.calculator", true);
+
+user_pref("browser.urlbar.suggest.addons", false);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.pocket", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.addons.featureGate", false);
+user_pref("browser.urlbar.mdn.featureGate", false);
+user_pref("browser.urlbar.pocket.featureGate", false);
+user_pref("browser.urlbar.weather.featureGate", false);
+user_pref("browser.urlbar.yelp.featureGate", false);
+
+
+////////
+// UI //
+////////
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"_testpilot-containers-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"redirector_einaregilsson_com-browser-action\",\"_20fc2e06-e3e4-4b2b-812b-ab431220cada_-browser-action\",\"leechblockng_proginosko_com-browser-action\",\"dfyoutube_example_com-browser-action\",\"firenvim_lacamb_re-browser-action\",\"_forest-firefox-addon-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_34daeb50-c2d2-4f14-886a-7160b24d66a4_-browser-action\",\"_f209234a-76f0-4735-9920-eb62507a54cd_-browser-action\",\"song-id_losnappas-browser-action\",\"simple-translate_sienori-browser-action\",\"_036a55b4-5e72-4d05-a06c-cba2dfcc134a_-browser-action\",\"_76ef94a4-e3d0-4c6f-961a-d38a429a332b_-browser-action\",\"_799c0914-748b-41df-a25c-22d008f9e83f_-browser-action\",\"bypasspaywalls_bypasspaywalls-browser-action\",\"jid1-zadieub7xozojw_jetpack-browser-action\",\"firefox_tampermonkey_net-browser-action\",\"_af37054b-3ace-46a2-ac59-709e4412bec6_-browser-action\",\"_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action\",\"treestyletab_piro_sakura_ne_jp-browser-action\",\"_bc2166c4-e7a2-46d5-ad9e-342cef57f1f7_-browser-action\",\"_edfc63b3-fc9b-4b6b-b9bf-4561ad548044_-browser-action\",\"jid1-bofifl9vbdl2zq_jetpack-browser-action\",\"wayback_machine_mozilla_org-browser-action\",\"idcac-pub_guus_ninja-browser-action\",\"_4c421bb7-c1de-4dc6-80c7-ce8625e34d24_-browser-action\",\"_702997ca-bea6-421e-888a-912d5e79a2ec_-browser-action\",\"_2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c_-browser-action\",\"_c0f619ac-25fb-43b6-954d-823fef95c834_-browser-action\",\"_dfe3bd7c-ba9b-42eb-94d5-4eefa422590f_-browser-action\",\"_59c55aed-bdb3-4f2f-b81d-27011a689be6_-browser-action\",\"firefoxcolor_mozilla_com-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\",\"unified-extensions-button\",\"reset-pbm-toolbar-button\",\"_fa247c57-77ac-41cd-b942-332051e15ced_-browser-action\",\"_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"ublock0_raymondhill_net-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_34daeb50-c2d2-4f14-886a-7160b24d66a4_-browser-action\",\"dfyoutube_example_com-browser-action\",\"_f209234a-76f0-4735-9920-eb62507a54cd_-browser-action\",\"song-id_losnappas-browser-action\",\"simple-translate_sienori-browser-action\",\"_036a55b4-5e72-4d05-a06c-cba2dfcc134a_-browser-action\",\"_76ef94a4-e3d0-4c6f-961a-d38a429a332b_-browser-action\",\"_forest-firefox-addon-browser-action\",\"_799c0914-748b-41df-a25c-22d008f9e83f_-browser-action\",\"bypasspaywalls_bypasspaywalls-browser-action\",\"jid1-zadieub7xozojw_jetpack-browser-action\",\"firefox_tampermonkey_net-browser-action\",\"profiler-button\",\"_af37054b-3ace-46a2-ac59-709e4412bec6_-browser-action\",\"leechblockng_proginosko_com-browser-action\",\"firenvim_lacamb_re-browser-action\",\"_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action\",\"treestyletab_piro_sakura_ne_jp-browser-action\",\"_testpilot-containers-browser-action\",\"_bc2166c4-e7a2-46d5-ad9e-342cef57f1f7_-browser-action\",\"_20fc2e06-e3e4-4b2b-812b-ab431220cada_-browser-action\",\"redirector_einaregilsson_com-browser-action\",\"_fa247c57-77ac-41cd-b942-332051e15ced_-browser-action\",\"_edfc63b3-fc9b-4b6b-b9bf-4561ad548044_-browser-action\",\"jid1-bofifl9vbdl2zq_jetpack-browser-action\",\"wayback_machine_mozilla_org-browser-action\",\"_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action\",\"idcac-pub_guus_ninja-browser-action\",\"_4c421bb7-c1de-4dc6-80c7-ce8625e34d24_-browser-action\",\"_702997ca-bea6-421e-888a-912d5e79a2ec_-browser-action\",\"_2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c_-browser-action\",\"_c0f619ac-25fb-43b6-954d-823fef95c834_-browser-action\",\"_dfe3bd7c-ba9b-42eb-94d5-4eefa422590f_-browser-action\",\"_59c55aed-bdb3-4f2f-b81d-27011a689be6_-browser-action\",\"firefoxcolor_mozilla_com-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\",\"unified-extensions-area\"],\"currentVersion\":20,\"newElementCount\":16}");
+user_pref("browser.startup.page", 3);
+user_pref("browser.newtabpage.enabled", true);
+
+user_pref("findbar.highlightAll", true);
+
+// Reader
+user_pref("reader.color_scheme", "custom");
+user_pref("reader.content_width", 5);
+user_pref("reader.custom_colors.background", "#1d2021");
+user_pref("reader.custom_colors.foreground", "#ebdbb2");
+user_pref("reader.custom_colors.unvisited-links", "#458588");
+user_pref("reader.custom_colors.visited-links", "#b16286");
+
+
+//////////////
+// FEATURES //
+//////////////
+
+// Containers
+user_pref("privacy.userContext.enabled", true);
+user_pref("privacy.userContext.ui.enabled", true);
+
+user_pref("pdfjs.disabled", false);
+user_pref("pdfjs.enableScripting", false);
+
+//////////////////////
+// PRIVACY/SECURITY //
+//////////////////////
+user_pref("extensions.formautofill.creditCards.enabled", false);
+
+// Anti-Fingerprinting
+user_pref("privacy.resistFingerprinting", true);
+user_pref("dom.maxHardwareConcurrency", 2);
+user_pref("browser.search.geoip.url", "");
+user_pref("browser.search.region", "GB");
+user_pref("browser.search.countryCode", "GB");
+user_pref("intl.accept_languages", "en-GB, en");
+user_pref("intl.locale.matchOS", false);
+
+// Disable APIs
+user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+user_pref("browser.search.geoSpecificDefaults", false);
+user_pref("geo.provider.ms-windows-location", false);
+user_pref("geo.provider.use_corelocation", false);
+user_pref("geo.provider.use_gpsd", false);
+user_pref("geo.provider.use_geoclue", false);
+
+user_pref("dom.netinfo.enabled", false);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.telephony.enabled", false);
+user_pref("beacon.enabled", false);
+user_pref("media.webspeech.recognition.enable", false);
+user_pref("device.sensors.enabled", false);
+user_pref("browser.send_pings", false);
+user_pref("browser.send_pings.require_same_host", true);
+user_pref("camera.control.face_detection.enabled", false);
+user_pref("clipboard.autocopy", false);
+
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.contentblocking.category", "strict"); // [HIDDEN PREF]
+
+
+// Tracking
+user_pref("browser.contentblocking.category", "strict");
+user_pref("privacy.donottrackheader.enabled", true);
+
+
+// Ads/suggestions
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.suggest.enabled.private", true);
+
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.shopping.experience2023.enabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+
+// Telemetry
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+
+user_pref("toolkit.telemetry.pioneer-new-studies-available", false);
+
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+
+
+/////////////////
+// PERFORMANCE //
+/////////////////
+//user_pref("content.notify.interval", 100000);
+//
+//// Caching
+//user_pref("gfx.canvas.accelerated.cache-items", 4096);
+//user_pref("gfx.canvas.accelerated.cache-size", 512);
+//user_pref("gfx.content.skia-font-cache-size", 20);
+//user_pref("browser.cache.jsbc_compression_level", 3);
+//user_pref("media.memory_cache_max_size", 65536);
+//user_pref("media.cache_readahead_limit", 7200);
+//user_pref("media.cache_resume_threshold", 3600);
+//user_pref("image.mem.decode_bytes_at_a_time", 32768);
+//
+//// Network
+//user_pref("network.http.max-connections", 1800);
+//user_pref("network.http.max-persistent-connections-per-server", 10);
+//user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+//user_pref("network.http.pacing.requests.enabled", false);
+//user_pref("network.dnsCacheExpiration", 3600);
+//user_pref("network.ssl_tokens_cache_capacity", 10240);
+
+
+// 2800 / 4500
